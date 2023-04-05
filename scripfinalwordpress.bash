dd4904fdb0a4c708485d65c981a62253b8fa6c52
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-04-05 16:46:29 +0200

    Update scripfinalwordpress.bash

--- scripfinalwordpress.bash
+++ scripfinalwordpress.bash
@@ -1,21 +1,27 @@
 #!/bin/bash
+# This a script to deploy a fonctionnal web-server with wordpress connected to an Azure Mariadb service (SAAS).
+
+# We install a lamp-server so we can get apache and php directly.
 sudo apt update && sudo apt install lamp-server^ -y
+# mariadb installation
 curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | sudo bash
 sudo apt-get install mariadb-server mariadb-client -y
 sudo apt install wordpress -y
 chown -R www-data:www-data wordpress/
 chmod -R 755 wordpress/
 sudo service mysql restart
+# wp-cli tool install to interact with wordpress with specific commands
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 php wp-cli.phar --info
 sudo chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
 wp --info
 sudo wp cli update
-#Désactiver SSL et autoriser connection NAT gateway dans les firewall rules pour que la ligne suivante fonctionne
+# Login to our SAAS mariadb to create a database
 sudo mariadb --user=rooTomadmin@tomdbmaria7 --password=@Azurev69007 --host=tomdbmaria7.mariadb.database.azure.com -e "create $
 cd /
 cd usr/share/wordpress
+# wp-config.php configuration in relation to our SAAS database
 sudo cp -p wp-config-sample.php /home
 sudo mv /home/wp-config-sample.php /usr/share/wordpress/wp-config.php
 sudo echo "define('FS_METHOD','direct');" >> wp-config.php
