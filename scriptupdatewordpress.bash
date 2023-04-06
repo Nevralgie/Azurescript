7ba1aa59ba33128d96d87a989beab6d87e9774fb
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-04-06 18:20:11 +0200

    Update scriptupdatewordpress.bash

--- scriptupdatewordpress.bash
+++ scriptupdatewordpress.bash
@@ -1,7 +1,8 @@
 #!/bin/bash 
+# Updating wordpress through your ubuntu terminal. Edit the wordpress directory path with your own. 
 
 sudo apt install unzip
-sudo rsync -Waq /var/www/html/wordpress  /home/azuretom
+sudo rsync -Waq /var/www/html/wordpress /home/azuretom
 sudo rm -rf /var/www/html/wordpress/wp-includes
 sudo rm -rf /var/www/html/wordpress/wp-admin
 sudo rm -rf /var/www/html/wordpress/wp-content
@@ -9,4 +10,4 @@ cd /tmp/ && wget https://wordpress.org/latest.zip
 unzip latest.zip
 sudo mv /tmp/wordpress/* /var/www/html/wordpress
 
-# Then go to http://www.example.com/wordpress/wp-admin/upgrade.php to initiate the upgrade, or adapt the URL to you wordpress directory path
+# Then go to http://www.example.com/wordpress/wp-admin/upgrade.php to automatically initiate the update. 
